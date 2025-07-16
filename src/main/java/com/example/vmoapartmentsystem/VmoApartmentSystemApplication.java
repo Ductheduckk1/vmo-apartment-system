9c3acf7eb018ad7b28b37c181d7bb45edc2e35b3
DucTheDuckk1 <a11duc1911@gmail.com>
2025-07-16 11:33:51 +0700

    init

---
+++ src/main/java/com/example/vmoapartmentsystem/VmoApartmentSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.vmoapartmentsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VmoApartmentSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VmoApartmentSystemApplication.class, args);
+    }
+
+}
